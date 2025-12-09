7beeac6b96f2f7fa8bd918ce670024ba3e94d461
jpulattie <jpulattie@gmail.com>
2025-12-09 13:35:31 -0600

    pushing demo code

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    words = "Hello World"
+    return render_template("index.html", words=words)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
